f802d8bcac7715bed0b427ee27dd3a5a85c5ce22
YANG WANG <yang.wang.dec@gmail.com>
2017-08-18 21:56:01 +0800

    vr test sound generates cubes

---
+++ Assets/ScaleMe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleMe : MonoBehaviour {
+
+    public GameObject OSC;
+
+    float angle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float income = OSC.GetComponent<oscReceive>().incomeValue;
+
+        //transform.localScale = new Vector3(income * 300, income * 300, income * 300);
+
+        transform.RotateAround(transform.position,new Vector3(Random.Range(-1,1),Random.Range(-1, 1),Random.Range(-1, 1)),angle);
+
+        if(income > 0.1){
+            GameObject newcube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            newcube.AddComponent<Rigidbody>();
+            newcube.transform.position = transform.position + Vector3.down;
+
+        }
+        angle += 0.01f;
+	}
+}
---
+++ Assets/osc/oscReceive.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class oscReceive : MonoBehaviour {
+    
+
+	private string UDPHost = "127.0.0.1";
+	private int listenerPort = 5001;
+	private int broadcastPort = 57131;
+	private Osc oscHandler;
+
+	public float incomeValue = 0;
+
+
+	void Start () {
+		
+		UDPPacketIO udp = GetComponent<UDPPacketIO>();
+		udp.init(UDPHost,broadcastPort,listenerPort);
+		oscHandler = GetComponent<Osc>();
+		oscHandler.init(udp);
+        oscHandler.SetAllMessageHandler(getInput);
+	}
+
+
+	void Update(){
+
+	}
+
+	public void getInput(OscMessage oscMessage) {
+		OscMessage m = oscMessage;
+
+		Osc.OscMessageToString(m);
+
+		if (m.Address == "/soundVol") {
+			incomeValue = (float)(m.Values [0]);
+		} 
+
+	}
+
+//	public void getTouchingForehead(OscMessage oscMessage) {
+//		Osc.OscMessageToString(oscMessage);
+//		touchingForehead = (int)(oscMessage.Values [0]);
+////		Debug.Log ("touching forehead: " + (int)(oscMessage.Values [0]));// Int32.Parse(oscMessage.Values[0]);
+//	}
+//
+//	public void getBatteryPer(OscMessage oscMessage){
+//		batteryPercentage = (int)oscMessage.Values[0];
+//		batteryPercentage = CustomFunc.Map (batteryPercentage, 0, 10000, 0, 100);
+////		Debug.Log ("battery%: "+ batteryPercentage);
+//	}
+//
+//	public void getBlink(OscMessage oscMessage){
+//		blink = (Boolean)oscMessage.Values[0];
+//		Debug.Log (blink);
+//	}
+
+
+}
